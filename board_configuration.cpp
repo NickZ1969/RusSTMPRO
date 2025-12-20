41978d1deed6ea5228ada49556f03347cfa8158e
NickZ1969 <z_nick_hay_z@hotmail.com>
2025-12-20 12:28:40 +1100

    Add files via upload

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -2,46 +2,67 @@
 #include "board_overrides.h"
 
 Gpio getCommsLedPin() {
-	return Gpio::Unassigned;
+	return Gpio::D15;
 }
 
 Gpio getRunningLedPin() {
-	return Gpio::Unassigned;
+	return Gpio::D12;
 }
 
 Gpio getWarningLedPin() {
-	return Gpio::Unassigned;
+	return Gpio::D13;
 }
 
 // board-specific configuration setup
 static void customBoardDefaultConfiguration() {
-    // engineConfiguration->injectionPins[0] = Gpio::F13;
-    // engineConfiguration->ignitionPins[0] = Gpio::E15;
+	engineConfiguration->canTxPin = Gpio::D1;
+	engineConfiguration->canRxPin = Gpio::D0;
+	
+    engineConfiguration->injectionPins[0] = Gpio::E6;
+	engineConfiguration->injectionPins[1] = Gpio::E5;
+	engineConfiguration->injectionPins[2] = Gpio::B9;
+	engineConfiguration->injectionPins[3] = Gpio::E3;
+	engineConfiguration->injectionPins[4] = Gpio::D9;
+	engineConfiguration->injectionPins[5] = Gpio::B14;
+	engineConfiguration->injectionPins[6] = Gpio::C13;
+	engineConfiguration->injectionPins[7] = Gpio::E14;
+	
+    engineConfiguration->ignitionPins[0] = Gpio::B13;
+	engineConfiguration->ignitionPins[1] = Gpio::E9;
+	engineConfiguration->ignitionPins[2] = Gpio::A8;
+	engineConfiguration->ignitionPins[3] = Gpio::E4;
+	engineConfiguration->ignitionPins[4] = Gpio::D3;
+	engineConfiguration->ignitionPins[5] = Gpio::D11;
+	engineConfiguration->ignitionPins[6] = Gpio::D10;
+	engineConfiguration->ignitionPins[7] = Gpio::D8;
 
-//   engineConfiguration->triggerInputPins[0] = Gpio::B1;
-//	engineConfiguration->triggerInputPins[1] = Gpio::Unassigned;
+	engineConfiguration->fanPin = Gpio::E7;
+	engineConfiguration->fuelPumpPin = Gpio::E8;
+	engineConfiguration->tachOutputPin = Gpio::E10;
+	engineConfiguration->vehicleSpeedSensorInputPin = Gpio::C7;
 
-//	engineConfiguration->map.sensor.hwChannel = EFI_ADC_3;
+	engineConfiguration->map.sensor.hwChannel = EFI_ADC_8;
+	engineConfiguration->clt.adcChannel = EFI_ADC_14;
+	engineConfiguration->iat.adcChannel = EFI_ADC_1;
+	engineConfiguration->tps1_1AdcChannel = EFI_ADC_15;
+	engineConfiguration->afr.hwChannel = EFI_ADC_2;
+	engineConfiguration->baroSensor.hwChannel = EFI_ADC_11;
 
-//	engineConfiguration->clt.adcChannel = EFI_ADC_1;
+	/* Battery voltage */
+	engineConfiguration->vbattAdcChannel = EFI_ADC_9;
+	/* Vbat divider: 10K + 1K */
+	engineConfiguration->vbattDividerCoeff = 8.23;
 
-//	engineConfiguration->iat.adcChannel = EFI_ADC_2;
+	engineConfiguration->triggerInputPins[0] = Gpio::C6;
+	engineConfiguration->camInputs[0] = Gpio::A5;
 
+    engineConfiguration->analogInputDividerCoefficient = 1.56f;
+	engineConfiguration->adcVcc = 3.3f;
 
-    	// 5.6k high side/10k low side = 1.56 ratio divider
-  //  	engineConfiguration->analogInputDividerCoefficient = 1.56f;
-
-    	// 6.34k high side/ 1k low side
-//    	engineConfiguration->vbattDividerCoeff = (6.34 + 1) / 1;
-
-//	engineConfiguration->adcVcc = 3.3f;
-
-//	engineConfiguration->clt.config.bias_resistor = 2490;
-//	engineConfiguration->iat.config.bias_resistor = 2490;
+	engineConfiguration->clt.config.bias_resistor = 2490;
+	engineConfiguration->iat.config.bias_resistor = 2490;
 
 
-	// Battery sense on PA0
-//	engineConfiguration->vbattAdcChannel = EFI_ADC_0;
 }
 
 void setup_custom_board_overrides() {
