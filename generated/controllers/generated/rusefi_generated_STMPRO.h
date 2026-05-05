600279ccca8ab4fd804805652bf9c4684f88648b
GitHub git update Action <action@github.com>
2026-05-05 00:53:12 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_STMPRO.h
+++ generated/controllers/generated/rusefi_generated_STMPRO.h
@@ -2178,7 +2178,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.05.04.STMPRO.2093963260"
+#define TS_SIGNATURE "rusEFI main.2026.05.05.STMPRO.2093963260"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
