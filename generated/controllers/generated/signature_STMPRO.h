3c2495adcde28bf3ae30bf3601ea335f45522488
GitHub git update Action <action@github.com>
2026-06-05 01:07:15 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_STMPRO.h
+++ generated/controllers/generated/signature_STMPRO.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2555766935
-#define TS_SIGNATURE "rusEFI main.2026.06.04.STMPRO.2555766935"
+#define SIGNATURE_HASH 2198732008
+#define TS_SIGNATURE "rusEFI main.2026.06.05.STMPRO.2198732008"
